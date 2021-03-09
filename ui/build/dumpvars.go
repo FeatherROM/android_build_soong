f7819603ad78a797ae5b50c0c04124de29e4dc6b
Rashed Abdel-Tawab <rashed@linux.com>
2021-03-09 20:38:45 -0300

    soong: Add FEATHER_RELEASE to dumpvars
    Change-Id: I24b29442f041aec451a173db404f542060ac67d2
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -145,6 +145,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 
 // Variables to print out in the top banner
 var BannerVars = []string{
+	"FEATHER_RELEASE",
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
 	"PRODUCT_INCLUDE_TAGS",
